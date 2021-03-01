735af6fc610938feda52cf753e4fd8fa6dcd4902
Skill Jobs Developer <dev2@skill.jobs>
2021-03-01 16:56:31 +0600

    first commit

---
+++ phonebook/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+from .models import Phonebook
+
+admin.site.register(Phonebook)
+# Register your models here.
---
+++ phonebook/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Phonebook(models.Model):
+    pid = models.CharField(max_length=20)
+    name =  models.CharField(max_length=30)
+    number =  models.CharField(max_length=50)
+
+
+    def __str__(self):
+        return  self.name + '    ' +self.number
+
+# Create your models here.
